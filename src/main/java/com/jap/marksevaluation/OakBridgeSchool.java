4d57c4a53a726930a2bcc50795b9ccfda5849fb0
Karen Jebakany <Karen.Jebakany@in.niit.com>
2022-06-10 12:29:24 +0530

    Boilerplate added

---
+++ src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
@@ -0,0 +1,57 @@
+package com.jap.marksevaluation;
+public class OakBridgeSchool {
+
+    // Convert the given string name to upper case
+    public String convertToUpperCase(String name){
+       return null;
+    }
+
+    // Sort the given names in alphabetical order
+    public String[] sortInAlphabeticalOrder(String[] names){
+          return names;
+    }
+
+    // Find the shortest word in the list of surnames
+    public String shortestSurname(String[] surnames)
+    {
+        return " ";
+    }
+
+    // Check if any of the names in the array is repeated, if yes return true else false
+    public boolean checkIfNamesAreRepeated(String[] names){
+        boolean flag = false;
+
+        return flag;
+    }
+
+    // Concatenate the name and surname of the students
+    public String[] concatenateNameAndSurname(String[] names, String [] surnames){
+        String []  concatenatedNames = null;
+
+        return  concatenatedNames;
+    }
+
+    // Display the names
+    public void displayStudentNames(String[] namesArray){
+
+
+    }
+
+    public static void main(String[] args) {
+        OakBridgeSchool oakBridgeSchool = new OakBridgeSchool();
+        // Declare and Initialize values to the arrays
+        String[] studentNames = null;
+        String[] studentSurnames = null;
+        // Call the method to convert all the student names to upper case and display results
+
+        // Call the method to sort names in alphabetical order and display results
+
+        // Call the method to find the shortest surname and display results
+
+        // Call the method to check if two names in the array are the same and display results
+
+        // Call the method to concatenate name and surname with a space in between and display results
+
+
+    }
+}
